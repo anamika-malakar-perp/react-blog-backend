f0229e4a5e19522357cf80305b94ebbb053554a1
anamika-malakar <anamikamalakar999@gmail.com>
2023-03-27 15:02:52 +0530

    Commented the post method

--- Routes/Bollywood.js
+++ Routes/Bollywood.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bollywoodApi = require('../Api/Bollywood');
 
 const router = express.Router();
-router.post('/bollywood', bollywoodApi.bollywoodDataInsertToDb);
+// router.post('/bollywood', bollywoodApi.bollywoodDataInsertToDb);
 router.get('/bollywood', bollywoodApi.bollywoodDataFindFromDb);
 
 module.exports = router;
--- Routes/Food.js
+++ Routes/Food.js
@@ -2,7 +2,7 @@ const express = require('express');
 const foodApi = require('../Api/Food');
 
 const router = express.Router();
-router.post('/food', foodApi.foodDataInsertToDb);
+// router.post('/food', foodApi.foodDataInsertToDb);
 router.get('/food', foodApi.foodDataFindFromDb);
 
 module.exports = router;
